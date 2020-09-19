09fe7317b3159ba879185843bd65462b74a167a4
Je Bytes <itamazor@gmail.com>
2020-09-19 17:46:38 -0300

    No add two tag at select.

--- src/hooks/getTextSelection.tsx
+++ src/hooks/getTextSelection.tsx
@@ -2,9 +2,14 @@ export function useGetTextSelection() {
   function getTextSelection(tag: string) {
     const sel = window.getSelection && window.getSelection()
     if (sel && sel.rangeCount > 0) {
-      const range = document.getSelection()?.getRangeAt(0)
+      const range = sel.getRangeAt(0)
+
+      if (range.startContainer.hasChildNodes()) {
+        return
+      }
+
       const element = document.createElement(tag)
-      if (range !== undefined) {
+      if (range.startOffset !== range.endOffset) {
         range.surroundContents(element)
       }
     }
